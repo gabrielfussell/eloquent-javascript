88bf629984b50acb4563cd7d713a4acae91d3c93
Gabe Fussell <gabrielfussell@gmail.com>
2022-04-27 20:57:07 -0400

    Began chapter 8

--- script.js
+++ script.js
@@ -1,235 +1,34 @@
-//Chapter 7 Project: A Robot
-
-const roads = [
-    "Alice's House-Bob's House"
-    , "Alice's House-Cabin"
-    , "Alice's House-Post Office"
-    , "Bob's House-Town Hall"
-    , "Daria's House-Ernie's House"
-    , "Daria's House-Town Hall"
-    , "Ernie's House-Grete's House"
-    , "Grete's House-Farm"
-    , "Grete's House-Shop"
-    , "Marketplace-Farm"
-    , "Marketplace-Post Office"
-    , "Marketplace-Shop"
-    , "Marketplace-Town Hall"
-    , "Shop-Town Hall"
-];
-
-function buildGraph(edges) {
-    let graph = Object.create(null);
-
-    function addEdge(from , to) {
-    if(graph[from] == null) {
-        graph[from] = [to];
-    } else {
-        graph[from].push(to);
-    }
-    }
-
-    for(let [from, to] of edges.map(r => r.split("-"))) {
-        addEdge(from, to);
-        addEdge(to, from);
-    }
-    return graph;
-}
-
-const roadGraph = buildGraph(roads);
-const mailRoute = [
-    "Alice's House"
-    , "Cabin"
-    , "Alice's House"
-    , "Bob's House"
-    , "Town Hall"
-    , "Daria's House"
-    , "Ernie's House"
-    , "Grete's House"
-    , "Shop"
-    , "Grete's House"
-    , "Farm"
-    , "Marketplace"
-    , "Post Office"
-]
-
-class VillageState {
-    constructor(place, parcels) {
-        this.place = place;
-        this.parcels = parcels;
-    }
-
-    static random(parcelCount = 5) {
-        let parcels = [];
-        for(let i = 0; i < parcelCount; i++) {
-            let address = randomPick(Object.keys(roadGraph));
-            let place;
-            do {
-                place = randomPick(Object.keys(roadGraph));
-            } while (place == address);
-            parcels.push({place, address});
-        }
-        return new VillageState("Post Office", parcels);
-    }
-
-    move(destination) {
-        if(!roadGraph[this.place].includes(destination)) {
-            return this;
-        } else {
-            let parcels = this.parcels.map(p => {
-                if(p.place != this.place) return p;
-                return {place: destination, address: p.address};
-            }).filter(p => p.place != p.address);
-            return new VillageState(destination, parcels);
-        }
+function problemChild() {
+    "use strict";
+    
+    for(i = 0; i < 10; i ++) {
+        console.log("Happy happy");
     }
 }
 
-function randomPick(array) {
-    let choice = Math.floor(Math.random() * array.length);
-    return array[choice];
+//problemChild();
+
+//incorrectly writes to the global scope instead of creating a new person object
+//strict mode prevents this
+function Person(name) {
+    this.name = name;
 }
 
-function runRobot(state, robot, memory) {
-    for(let turn = 0;; turn++) {
-        if(state.parcels.length == 0) {
-            //console.log(`Done in ${turn} turns`);
-            return turn;
-        }
-        let action = robot(state, memory);
-        state = state.move(action.direction);
-        memory = action.memory;
-        //console.log(`Moved to ${action.direction}`);
+let ferdinand = Person("Ferdinand");
+//console.log(name); 
+
+function numberToString(n, base = 10) {
+    let result = "", sign = "";
+    if(n < 0) {
+        sign = "-";
+        n = -n;
     }
+    do {
+        result = String(n % base) + result;
+        //debugger;
+        n = Math.floor(n / base);
+    } while (n > 0);
+    return sign + result;
 }
 
-
-function randomRobot(state) {
-    return {direction: randomPick(roadGraph[state.place])};
-}
-
-function routeRobot(state, memory = []) {
-    if(memory.length == 0) {
-        memory = mailRoute;
-    }
-    return {direction: memory[0], memory: memory.slice(1)};
-}
-
-function findRoute(graph, from, to) {
-    let work = [{at: from, route: []}];
-
-    for(let i = 0; i < work.length; i++) {
-        let {at, route} = work[i];
-        for(let place of graph[at]) {
-            if(place == to) {
-                //console.log(work);
-                return route.concat(place);
-            }
-            if(!work.some(w => w.at == place)) {
-                work.push({at: place, route: route.concat(place)});
-            }
-        }
-    }
-}
-
-function goalOrientedRobot({place, parcels}, route = []) {
-    if(route.length == 0) {
-        let parcel = parcels[0];
-        if(parcel.place != place) {
-            route = findRoute(roadGraph, place, parcel.place);
-        } else {
-            route = findRoute(roadGraph, place, parcel.address);
-        }
-    }
-    return {direction: route[0], memory: route.slice(1)};
-}
-
-//console.log(roadGraph);
-//console.log(findRoute(roadGraph, "Alice's House", "Shop"));
-
-//runRobot(VillageState.random(), routeRobot);
-//let r = runRobot(VillageState.random(), goalOrientedRobot);
-//console.log(`Done in ${r} turns`);
-
-
-
-/**********
-Exercises
-**********/
-//measuring a robot
-function compareRobots(robot1, robot2, numTasks = 100) {
-    //track how many steps to finish each task
-    r1Log = [];
-    r2Log = [];
-
-    for(i = 0; i < numTasks; i++) {
-        let village = VillageState.random();
-
-        //runRobot function modified to return number of steps to complete
-        r1Log.push(runRobot(village, robot1));
-        r2Log.push(runRobot(village, robot2));
-    }
-
-    let r1Avg = r1Log.reduce((a, b) => a + b) / r1Log.length;
-    let r2Avg = r2Log.reduce((a, b) => a + b) / r2Log.length;
-
-    console.log(`Robot 1 Average: ${r1Avg}`);
-    console.log(`Robot 2 Average: ${r2Avg}`);
-}
-
-//compareRobots(routeRobot, goalOrientedRobot);
-
-//Persistent Group
-//explanation: https://stackoverflow.com/questions/56160350/eloquent-javascript-persistent-group
-class PGroup {
-    constructor(group) {
-        this.group = group;
-    }
-
-    add(value) {
-        let newPGroup = new PGroup();
-        newPGroup.group = this.group.map(x => x); //copy the original w/no changes
-        newPGroup.group.push(value);
-        return newPGroup;
-    }
-
-    delete(value) {
-        let newPGroup = new PGroup();
-        //copy everything to the new group except value we're "deleting"
-        newPGroup.group = this.group.filter(x => x !== value);
-        return newPGroup;
-    }
-
-    has(value) {
-        if(this.group.indexOf(value) == -1) {
-            return false;
-        } else {
-            return true;
-        }
-    }    
-}
-
-PGroup.empty = new PGroup([]);
-
-/*
-let a = new PGroup();
-let b = a.add("Flaming Hot Cheetos");
-let c = a.add("Grilled Cheese");
-let d = b.add("Please eat some vegetables");
-
-console.log(a.group);
-console.log(b.group);
-console.log(c.group);
-console.log(d.group);
-console.log(a.group);
-*/
-
-//PGroup.empty on line 210 is what allows for this construction
-let e = PGroup.empty.add("January").add("February").add("March").add("April").add("May");
-
-let f = e.delete("January");
-let g = e.delete("February");
-
-console.log(e.group);
-console.log(f.group);
-console.log(g.group);
-console.log(e.group);
+console.log(numberToString(13));
