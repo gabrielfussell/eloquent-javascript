f5da94a3364d5cc1362fef4132f8387087dfb2a0
Gabe Fussell <gabrielfussell@gmail.com>
2022-05-07 18:49:15 -0400

    Finished Chapter 8 exercises

--- script.js
+++ script.js
@@ -29,3 +29,44 @@ for(;;) {
     }
 }
 */
+
+/***********
+THE LOCKED BOX
+***********/
+
+const box = {
+    locked: true,
+    unlock() { this.locked = false; },
+    lock() { this.locked = true; },
+    _content: [],
+    get content() { 
+        if(this.locked) throw new Error("Locked!");
+        return this._content;
+    }
+};
+
+function withBoxUnlocked(body) {
+    let wasLocked = box.locked;
+    box.unlock();
+
+    try {
+        body();
+    } finally {
+        if(wasLocked) box.lock(); //re-lock the box only if it was locked to start with
+    }
+}
+
+withBoxUnlocked(function() {
+    box.content.push("gold piece");
+});
+
+//box.locked = false;
+try {
+    withBoxUnlocked(function() {
+        throw new Error("Pirates on the horizon! Abort!");
+    });
+} catch(e) {
+    console.log("Error raised:", e);
+}
+
+console.log(box.locked);
