347b0067faa45215b236e493dcdc3872ec9a860a
Gabe Fussell <gabrielfussell@gmail.com>
2022-05-15 21:03:15 -0400

    Continued work on regular expressions

--- script.js
+++ script.js
@@ -20,6 +20,9 @@ let escape2 = /ab\/c/;
 //console.log(/abc/.test("abcde")); //true
 //console.log(/abc/.test("abxde")); //false
 
+//Brackets indicate a set of characters. Any character within that set would pass the test (equivalent to "OR")
+let mySet = /[1234]/;
+
 //part of the expression matches any of the characters between the brackets
 //console.log(/[0123456789]/.test("in 1992"));
 //console.log(/[0-9]/.test("in 1992"));
@@ -59,5 +62,32 @@ let dateTimeFormat = /\d{1,2}-\d{1,2}-\d{4} \d{1,2}:\d{2}/;
 //console.log(dateTimeFormat.test("1-30-2003 8:45"));
 
 // Part of a regular expression encolsed in parentheses is counted as a single element
-let crying = /boo+(hoo+)+/i;
-console.log(crying.test("bobooboohoo"));
+let subExpressions = /boo+(hoo+)+/i;
+//console.log(subExpressions.test("bobooboohoo"));
+
+// exec() function returns null if no match was found
+// otherwise retuns an object with info about the match
+let regexMatch = /\d+/.exec("one two 100");
+//console.log(regexMatch);
+
+//Strings also have a match method that takes a regular expression which works similarly
+let stringMatch = "one two 100".match(/\d+/);
+//console.log(stringMatch);
+
+let groupMatchSingle = /'([^']*)'/; //match on quoted text
+//console.log(groupMatchSingle.exec("she said 'hello'"));
+let groupMatchNone = /bad(ly)?/;
+//console.log(groupMatchNone.exec("bad"));
+let groupMatchMultiple = /(\d)+/;
+//console.log(groupMatchMultiple.exec("123"));
+
+
+/****************
+DATES AND TIMES 
+*****************/
+let basicDateObject = new Date();
+//Months start at zero but days start at one!!!
+let specificDate = new Date(2005, 06, 01);
+
+console.log(specificDate);
+console.log(specificDate.getTime());
